445ccc1550950562dae8b390482501bf0674adf2
YevheniiaUhnich <140949326+YevheniiaUhnich@users.noreply.github.com>
2023-10-11 08:40:30 +0300

    hw1

--- js/task-3.js
+++ js/task-3.js
@@ -3,9 +3,11 @@ function getElementWidth(content, padding, border) {
     const paddingWidth = parseInt(padding);
     const borderThickness = parseInt(border);
     
-    return contentWidth + paddingWidth + borderThickness;
+    return contentWidth + 2 * paddingWidth + 2 * borderThickness;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
